637668e23bae261c22e567cfeb1043059cc90ff5
root <mutukumaxwel@gmail.com>
2023-10-05 16:45:39 -0700

    New task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+	char puzzle[] = "Programming is like building a multiligual puzzle\n";
+
+	puts(puzzle);
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+	char puzzle[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	puts(puzzle);
+	return (0);
+}
