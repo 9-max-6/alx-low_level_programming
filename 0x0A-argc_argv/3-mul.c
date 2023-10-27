fee34648e35e0fc0619ec79d04453330894145c8
9-max-6 <mutukumaxwel@gmail.com>
2023-10-27 20:52:02 +0300

    New task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -39,56 +39,21 @@ int _atoi(char *s)
 	return (sign * result);
 }
 /**
- * is_num - a function to check if each character of
- * a string is a digit.
- * @s: the string
- *
- * Return: returns 0 if okay: -1 if not
- */
-
-int is_num(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] > '9' || s[i] < '0')
-		{
-			return (-1);
-		}
-	}
-
-	return (0);
-}
-/**
- * main - print arg vector
- * @argc: length of argv
+ * main - a function to multiply two numbers.
+ * @argc: argv.len()
  * @argv: argument vector
+ *
  * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	int i, sum, temp;
-	
-	sum = 0;
-	for (i = 1; i < argc; i++)
+	if (argc != 3)
 	{
-		if (is_num(argv[i]) == -1)
-		{
-			printf("Error\n");
-			return (0);
-		}
+		printf("Error\n");
+		return (1);
 	}
-	
-	for (i = 1; i < argc; i++)
-	{
-		temp = _atoi(argv[i]);
-		if (temp >= 0)
-		{
-			sum += temp;
-		}
-	}
-	printf("%d\n", sum);
+
+	printf("%d\n", (_atoi(argv[1]) * _atoi(argv[2])));
 	return (0);
 }
