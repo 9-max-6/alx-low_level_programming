10e10c56d7380729e90875b3106482a86f1ebf02
9-max-6 <mutukumaxwel@gmail.com>
2023-10-27 19:27:44 +0300

    new task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,19 +2,6 @@
 #include <unistd.h>
 
 /**
- * _putchar - a function to write a character to
- * stdout
- * @c: the character
- *
- * Return: returns 1 on success.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
  * main - a function to print out its name
  * @argc: the number of arguments in arg vector
  * @argv: the argument vector
@@ -27,13 +14,8 @@ int main(__attribute__((unused))int argc, char *argv[])
 
 	int i;
 	i = 0;
-	while (argv[0][i] != '\0')
-	{
-		_putchar(argv[0][i]);
-		i++;
-		
-	}
-	_putchar('\n');
+	
+	printf("%s\n", *argv[0]);
 
 	return (0);
 }
