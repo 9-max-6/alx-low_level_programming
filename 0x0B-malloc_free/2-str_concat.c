21894d75e7e906820b32949be357ff02cfd037b8
9-max-6 <mutukumaxwel@gmail.com>
2023-11-01 00:12:29 +0300

    new task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,13 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j;
 	unsigned int size;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s2 == NULL && s1 != NULL))
 	{
-		return (NULL);
+		return (s1)
+	}
+	if (s2 != NULL && s1 == NULL)
+	{
+		return (s1);
 	}
 	for (i = 0; s1[i] != '\0'; i++)
 		;
