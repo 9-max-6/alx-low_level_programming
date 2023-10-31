37b26a3ca3bbcb4dda61c8534e36992bff3fddc9
9-max-6 <mutukumaxwel@gmail.com>
2023-11-01 00:31:30 +0300

    New task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,48 @@
 #include "main.h"
 
 /**
+ * _strlen - a function determine the length of a string
+ * @str: pointer to the string.
+ * 
+ * Return: returns the length of the string or NULL
+ */
+
+int _strlen(char *str)
+{
+	int j; 
+
+	if (!str)
+	{
+		return (0);
+	}
+	for (j = 0; str[j] != '\0'; j++)
+		;
+
+	return (j);
+}
+/**
+ * sing_str - a function to duplicate single string
+ * @str: the string to be duped.
+ * Return: pointer to the string.
+ */
+
+char *sing_str(char *str)
+{
+	int j, i;
+	char *new_str;
+
+	j = _strlen(str);
+	new_str =  malloc(sizeof(char) * (j + 1));
+	
+	for (i = 0; i < j; i++)
+	{
+		new_str[i] = str[i];
+	}
+
+	return (new_str);
+}	
+
+/**
  * str_concat - a function to concatenate two strings.
  * @s1: the first string.
  * @s2: the second string.
@@ -16,11 +58,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s2 == NULL && s1 != NULL)
 	{
-		return (s1);
+		return (sing_str(s1));
 	}
 	if (s2 != NULL && s1 == NULL)
 	{
-		return (s2);
+		return (sing_str(s2));
 	}
 	if (s1 == NULL && s2 == NULL)
 	{
