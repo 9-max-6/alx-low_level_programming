d8d256f47f1dfb04b0e0372581d9d86a12f1399d
9-max-6 <mutukumaxwel@gmail.com>
2023-11-03 20:07:28 +0300

    New task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,29 +18,27 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int  new_size)
 	{
 		return (ptr);
 	}
-	new_ptr = malloc(new_size);
-	if (!new_ptr)
-	{
-		return (NULL);
-	}
 	if (!ptr)
 	{
-		return (new_ptr);
+		new_ptr = malloc(new_size);
+		if (!new_ptr)
+			return (NULL);
+		else
+			return (new_ptr);
 	}
-
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		ptr = NULL;
 		return (NULL);
 	}
+	n
 	copy_size = (old_size < new_size) ? old_size : new_size;
 	new_ptr = memcpy(new_ptr, ptr, copy_size);
-	
+
 	if (!new_ptr)
 	{
 		return (NULL);
 	}
 	return (new_ptr);
 }
-
