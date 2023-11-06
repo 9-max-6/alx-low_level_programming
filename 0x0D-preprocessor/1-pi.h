bc0cb2afef3ae67bde3007d6ff7460f36ab6bc16
9-max-6 <mutukumaxwel@gmail.com>
2023-11-06 22:33:16 +0300

    New task

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef __MAIN__
+#define __MAIN__
+
+#define PI 3.14159265359
+
+#endif
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef __MAIN__
+#define __MAIN__
+
+#define ABS(x) (((x) < 0) ? ((-1) * (x)) : (x))
+
+#endif
