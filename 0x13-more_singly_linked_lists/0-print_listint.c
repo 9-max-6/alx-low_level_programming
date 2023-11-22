ff725239322e1aec78739d5adbc3bf4304cc10f2
root <mutukumaxwel@gmail.com>
2023-11-21 16:47:19 -0800

    New task

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - a function to print a listint structure
+ * @h: pointer to the head  list.
+ *
+ * Return: returns the number of nodes.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	unsigned int node_ct = 0;
+	
+	if (!h)
+		return (node_ct);
+	while(h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (node_ct);
+}
